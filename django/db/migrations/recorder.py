1a1d29bb98b5214cc090735bf5de1bcf0449e117
Martin Ogden <martin.ogden@gmail.com>
2018-08-14 16:26:39 +0100

    disable creation of migration table

--- django/db/migrations/recorder.py
+++ django/db/migrations/recorder.py
@@ -41,22 +41,13 @@ class MigrationRecorder(object):
 
     @property
     def migration_qs(self):
-        return self.Migration.objects.using(self.connection.alias)
+        return self.Migration.objects.using(self.connection.alias).none()
 
     def ensure_schema(self):
         """
         Ensures the table exists and has the correct schema.
         """
-        # If the table's there, that's fine - we've never changed its schema
-        # in the codebase.
-        if self.Migration._meta.db_table in self.connection.introspection.table_names(self.connection.cursor()):
-            return
-        # Make the table
-        try:
-            with self.connection.schema_editor() as editor:
-                editor.create_model(self.Migration)
-        except DatabaseError as exc:
-            raise MigrationSchemaMissing("Unable to create the django_migrations table (%s)" % exc)
+        return
 
     def applied_migrations(self):
         """
